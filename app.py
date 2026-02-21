98f1cc5f86f5dd414a485fe78c2fed148f2b0a7c
Vinoth Nachimuthu Manoharan <36736656+nmvinoth@users.noreply.github.com>
2026-02-21 15:44:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -448,9 +448,9 @@ def make_invoice_pdf(
     kv("Description of Service Accounting Code (SAC)", person.desc, extra_after=16)
     # Location (force single line)
     draw_txt(label_x, y, "Location of Service Provided", size=10, bold=False)
-    draw_txt(colon_x, y, ":", size=9, bold=False, col=colors.HexColor("#666666"))
+    draw_txt(colon_x, y, ":", size=10, bold=False, col=colors.HexColor("#666666"))
     
-    draw_txt(value_x, y, person.location, size=10, bold=False)
+    draw_txt(value_x, y, person.location, size=9, bold=False)
     y -= 20
     kv("State code of service location", person.state_code, extra_after=10)
     kv("State name of service location", person.state_name, extra_after=12)
@@ -790,6 +790,7 @@ st.download_button(
     mime="application/pdf",
     use_container_width=True
 )
+
 
 
 
